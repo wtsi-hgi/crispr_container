bcd00fac8cd32f3a5ba0dc266cbef8280f3ac3d2
gn5 <gn5@sanger.ac.uk>
2021-04-12 15:03:06 +0000

    auto-git

--- get_from_dockerhub.sh
+++ get_from_dockerhub.sh
@@ -1,14 +1,14 @@
-#!/usr/bin/env bash                                                                                                                         
-                                                                                                                                            
-mkdir -p cache_dir                                                                                                                          
-export SINGULARITY_CACHEDIR=$PWD/cache_dir                                                                                                  
-                                                                                                                                            
-mkdir -p tmp_dir                                                                                                                            
-export TMPDIR=$PWD/tmp_dir                                                                                                                  
-                                                                                                                                            
-# see https://github.com/wtsi-hgi/scrna_deconvolution_container                                                                             
-rm -f scrna_deconvolution_latest.sif                                                                                                        
-singularity pull docker://mercury/scrna_deconvolution      
+#!/usr/bin/env bash
+
+mkdir -p cache_dir
+export SINGULARITY_CACHEDIR=$PWD/cache_dir
+
+mkdir -p tmp_dir
+export TMPDIR=$PWD/tmp_dir
+
+# see https://github.com/wtsi-hgi/nextflow_rna_seq_container
+rm -f rna_seq.sif
+singularity pull docker://wtsihgi/rna_seq
 
 rm -r cache_dir
 rm -r tmp_dir
