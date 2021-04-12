abccc4ed7c37c9a9da83a3d4d8e009c6fd658887
gn5 <gn5@sanger.ac.uk>
2021-04-12 15:27:55 +0000

    auto-git

--- get_from_dockerhub.sh
+++ get_from_dockerhub.sh
@@ -7,8 +7,8 @@ mkdir -p tmp_dir
 export TMPDIR=$PWD/tmp_dir
 
 # see https://github.com/wtsi-hgi/nextflow_rna_seq_container
-rm -f rna_seq.sif
-singularity pull docker://wtsihgi/rna_seq
+rm -f rna_seq_1.0.sif
+singularity pull docker://wtsihgi/rna_seq:1.0
 
 rm -r cache_dir
 rm -r tmp_dir
